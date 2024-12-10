3290f62f3df309cd1088118354d31087f16ff4a3
MPNose <mpnoseworthy89@gmail.com>
2024-12-10 12:58:43 -0800

    exercise 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alice", last_name: "Johnson", hourly_rate: 55)
+@store1.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 45)
+@store1.employees.create(first_name: "Charlie", last_name: "Brown", hourly_rate: 50)
+@store1.employees.create(first_name: "Diana", last_name: "Green", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Emma", last_name: "Davis", hourly_rate: 65)
+@store2.employees.create(first_name: "Frank", last_name: "Miller", hourly_rate: 48)
+@store2.employees.create(first_name: "Grace", last_name: "Wilson", hourly_rate: 52)
+@store2.employees.create(first_name: "Henry", last_name: "Moore", hourly_rate: 58)
+@store2.employees.create(first_name: "Isla", last_name: "Taylor", hourly_rate: 54)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "please enter a store name"
+
+store_name = gets.chomp  
+
+
+store = Store.new(name: store_name)
+
+
+if store.save
+  puts "Store '#{store_name}' was created successfully."
+else
+ 
+  puts "Error: Could not create store."
+  store.errors.full_messages.each do |message|
+    puts message  
+  end
+end
